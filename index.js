6e03aef3ed97eb0bdbec7d5f8483662eca55b8f9
SamourM <48651399+SamourM@users.noreply.github.com>
2019-04-11 12:36:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var AWS = require('aws-sdk')
 AWS.config.loadFromPath('./crd.json');
 
 var ec2 = new AWS.EC2();
-var params = {
-    InstanceIds: [
+
+/*var params = {
+    InstanceIds: [] }; */
        
-    ]
-   };
+    
+   
    ec2.describeInstances(function(err, data) {
      if (err) console.log(err, err.stack); // an error occurred
      else     console.log(JSON.stringify(data));           // successful response
